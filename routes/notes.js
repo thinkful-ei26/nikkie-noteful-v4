374e83758d494c310d94fd02cc5897906c51af96
Nikkie Mashian <nikkiemashian@gmail.com>
2018-11-30 10:22:53 -0800

    Update /routes/notes.js endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -9,14 +9,18 @@ const router = express.Router();
 
 /* ========== GET/READ ALL NOTES ========== */
 router.get('/', (req, res, next) => {
-  const { searchTerm, folderId } = req.query;
+  const { searchTerm, folderId, tagId } = req.query;
 
   const filter = {}; 
 
   if (folderId) {
     filter.folderId =  folderId;
   }
-  // Check if request contains folderId in the querystring and add a filter which to find notes with the given folderId QUESTION when is this used? When user clicks folder and wants to see notes in that folder?...YES look at network for the requests you'll see
+
+  if(tagId){
+    filter.tags =  tagId;
+  } //QUUESTION: WHY NOT filter.tagId = tagId 
+  // Check if request contains folderId in the querystring and add a filter which to find notes with the given folderId QUESTION when is this used? When user clicks folder and wants to see notes in that folder?...YES look at network for the requests you'll see. Same with tags.
 
 
   if (searchTerm) {
@@ -24,7 +28,9 @@ router.get('/', (req, res, next) => {
     filter.$or = [{ 'title': re }, { 'content': re }];
   }
 
-  Note.find(filter).sort({ updatedAt: 'desc' })
+  Note.find(filter)
+    .populate('tags') //QUESTION why is tags first in the response?
+    .sort({ updatedAt: 'desc' })
     .then(notes => {
       res.json(notes);
     })
@@ -45,6 +51,7 @@ router.get('/:id', (req, res, next) => {
   }
 
   Note.findById(id)
+    .populate('tags')
     .then(notes => {
       if(notes){
         res.json(notes);
@@ -61,7 +68,7 @@ router.get('/:id', (req, res, next) => {
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
   //Check if request contains a folderId and verify it is a valid Mongo ObjectId, if not valid respond with an error
-  const { title, content, folderId } = req.body;
+  const { title, content, folderId, tags =[] } = req.body;
 
   // if there's a folderid, make sure its a valid mongoose objectid
   if (folderId && !mongoose.Types.ObjectId.isValid(folderId)) {
@@ -69,6 +76,14 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
+
+  tags.forEach(tagId=>{
+    if (!mongoose.Types.ObjectId.isValid(tagId)) {
+      const err = new Error('The `id` is not valid');
+      err.status = 400;
+      return next(err);
+    }
+  });
 
   // Make sure user included title and content 
   const requiredFields = [title, content];
@@ -87,6 +102,9 @@ router.post('/', (req, res, next) => {
   if (folderId){
     newNote.folderId = folderId;
   }
+  if(tags){
+    newNote.tags = tags;
+  }
 
   Note.create(newNote)
     .then(note => {
@@ -100,8 +118,7 @@ router.post('/', (req, res, next) => {
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
   const{ id }= req.params;
-  const { title, content, folderId } = req.body;
-  console.log(req.body);
+  const { title, content, folderId, tags =[] } = req.body;
   
   // validate id
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -117,11 +134,20 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
+  console.log('The tags are', tags);
+  tags.forEach(tagId=>{
+    console.log('the tag id is:', tagId);
+    if (!mongoose.Types.ObjectId.isValid(tagId)) {
+      const err = new Error('The `id` is not valid');
+      err.status = 400;
+      return next(err);
+    }
+  });
+
   // Make sure user included title and content 
   const requiredFields = [title, content];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
-    console.log(req.body);
     if (!field) {
       const err = new Error ('Must have title and content!');
       err.status = 400;
@@ -132,7 +158,7 @@ router.put('/:id', (req, res, next) => {
 
   //only want to put in the folderId if there is one or else it gets a cast error! QUESTION: didn't have a problem with this in post, but it does in put? 
   // In the solution for the "PUT" Notes endpoint, it says "const updateNote = { title, content, folderId }". But if folderId doesn't exist since the user didn't choose a folder when updating, when we try to update a note with folderId = '', it throws a CastError. To avoid this, I changed the code to this: 
-  const updateNote = {title, content};
+  const updateNote = {title, content, tags};
   if (folderId){
     updateNote.folderId = folderId;
   }
