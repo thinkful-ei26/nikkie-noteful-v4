72025a2f90c16f4e776fb0014ad766aa7c38ae3e
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-03 12:12:48 -0800

    Create User schema and model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  username: {type: String, unique: true, required: true}.required,
+  password: {type: String, required: true},
+});
+
+// This is like using a serialize method
+userSchema.set('toJSON', {
+  virtuals: true,     // include built-in virtual `id`
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password; //dont want to give back the password in the response!
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
